1852657c46bce338449256e20181a468c7af23e4
VL <vlewin@macbook.fritz.box>
2016-02-16 22:56:35 +0100

    init commit

---
+++ vue/inline.js
@@ -0,0 +1,13 @@
+// var source   = $('#entry-template').html();
+// console.log(source);
+// var template = Handlebars.compile(source);
+//
+// console.log(template);
+// var context = { title: 'My New Post', body: 'This is my first post!' };
+//
+// console.log(context);
+// var html = template(context);
+//
+// console.log(html);
+//
+// $('result').html(html);
